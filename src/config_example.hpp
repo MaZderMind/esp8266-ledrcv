921aad3a7701526ab412829099b1c27eafd2dc7b
MaZderMind <git@mazdermind.de>
2021-07-01 17:02:06 +0200

    structure

--- src/config_example.hpp
+++ src/config_example.hpp
@@ -3,6 +3,6 @@
 static const int16_t udpPort = 5555;
 static const int16_t numLeds = 32;
 
-static const char basename[] = "Blinky";
+static const char devicename[] = "Blinky";
 static const char ssid[] = "Your";
 static const char password[] = "WLan";
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 WiFiUDP Udp;
 
+uint8_t packetBuffer[0x0FFF];
+
 static void constructHostname(const char *prefix, char *hostnameBuffer,
                               size_t bufferSize) {
   strlcpy(hostnameBuffer, prefix, bufferSize);
@@ -18,12 +20,9 @@ static void constructHostname(const char *prefix, char *hostnameBuffer,
   strlcat(hostnameBuffer, chipIdAsHex, bufferSize);
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.print(F("\n\n\n"));
-
+void setupWifi() {
   char hostname[32];
-  constructHostname(basename, hostname, sizeof(hostname));
+  constructHostname(devicename, hostname, sizeof(hostname));
   Serial.print(F("Hostname:"));
   Serial.println(hostname);
   Serial.print(F("MAC: "));
@@ -42,20 +41,22 @@ void setup() {
   Serial.print(F("Connected!\n"));
   Serial.print(F("IP: "));
   Serial.println(WiFi.localIP());
+}
 
+void setupUdpSocket() {
   Serial.print(F("Listening for Datagrams on Port "));
   Serial.println(udpPort);
   Udp.begin(udpPort);
 }
 
-void loop() {
-  uint8_t packetBuffer[0xFFFF];
-
+bool readUdpPacket() {
   int packetSize = Udp.parsePacket();
   if (packetSize > 0) {
-    Serial.print(F("Received Datagram of"));
-    Serial.print(packetSize);
-    Serial.println(F(" bytes"));
+    if (debugCommunication) {
+      Serial.print(F("Received Datagram of"));
+      Serial.print(packetSize);
+      Serial.println(F(" bytes"));
+    }
 
     // read the packet into packetBufffer
     packetBuffer[0] = 0x00;
@@ -64,10 +65,30 @@ void loop() {
       packetBuffer[len] = 0x00;
     }
 
-    Serial.print(F("Contents:"));
-    for (int i = 0; i < len; i++) {
-      Serial.printf(" %02X", packetBuffer[i]);
+    if (debugCommunication) {
+      Serial.print(F("Contents:"));
+      for (int i = 0; i < len; i++) {
+        Serial.printf(" %02X", packetBuffer[i]);
+      }
+      Serial.println(F("."));
     }
-    Serial.println(F("."));
+  }
+
+  return packetSize > 0;
+}
+
+void processCommand() { Serial.println(F("Executing Command")); }
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print(F("\n\n\n"));
+
+  setupWifi();
+  setupUdpSocket();
+}
+
+void loop() {
+  if (readUdpPacket()) {
+    processCommand();
   }
 }
